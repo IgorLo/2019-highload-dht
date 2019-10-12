9b1b22e6c1e8e3bcc441e9b19311e798c4570ff5
igorlo <9440533@gmail.com>
2019-10-12 16:22:18 +0300

    updated testing script

--- scripts/wrk_aprof.sh
+++ scripts/wrk_aprof.sh
@@ -3,6 +3,9 @@
 #JPID= ps ux | grep '/snap/intellij-idea-ultimate/173/jbr/bin/java -Xmx128M' | grep -v grep | awk '{print $2}\'
 #echo $JPID
 
+#lover paranoidness of system
+sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
+
 TIME=10
 
 if [[ "$1" != "" ]]; then
@@ -37,21 +40,21 @@ aprof stop jps
 #Hardcore tests
 
 wrk -s get.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080
-aprof start -f output/1_get_empty.svg jps
-wrk -s get.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080 >> output/1_get_empty_wrk.txt
+aprof start -f output/1_get_empty_million.svg jps
+wrk -s get.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080 >> output/1_get_empty_wrk_million.txt
 aprof stop jps
 
 wrk -s put.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080
-aprof start 99 -f output/2_put.svg jps
-wrk -s put.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080 >> output/2_put_wrk.txt
+aprof start 99 -f output/2_put_million.svg jps
+wrk -s put.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080 >> output/2_put_wrk_million.txt
 aprof stop jps
 
 wrk -s get.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080
-aprof start 99 -f output/3_get_full.svg jps
-wrk -s get.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080 >> output/3_get_full_wrk.txt
+aprof start 99 -f output/3_get_full_million.svg jps
+wrk -s get.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080 >> output/3_get_full_wrk_million.txt
 aprof stop jps
 
 wrk -s delete.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080
-aprof start 99 -f output/4_delete.svg jps
-wrk -s delete.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080 >> output/4_delete_wrk.txt
+aprof start 99 -f output/4_delete_million.svg jps
+wrk -s delete.lua -c 4 -d $TIME -t 4 -R 1000k -L http://localhost:8080 >> output/4_delete_wrk_million.txt
 aprof stop jps
