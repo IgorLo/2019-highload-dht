b905cadc0ea52c775bc0e82457131e2ef9d3b4d5
igorlo <9440533@gmail.com>
2019-10-22 15:31:37 +0300

    починил async-profiler добавил результаты профилирования

--- scripts/wrk_aprof.sh
+++ scripts/wrk_aprof.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#JPID= ps ux | grep '/snap/intellij-idea-ultimate/173/jbr/bin/java -Xmx128M' | grep -v grep | awk '{print $2}\'
-#echo $JPID
-
-JPID=16932
+JPID=$(ps ux | grep '/snap/intellij-idea-ultimate/173/jbr/bin/java -Xmx128M' | grep -v grep | awk '{print $2}\')
+echo $JPID
 
 #lover paranoidness of system
 sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
@@ -20,24 +18,26 @@ rm -rf output
 mkdir output
 chmod 777 output
 
+aprof stop "$JPID"
+
 # Разогрев
-#wrk -s get.lua -c 4 -d 30 -t 4 -R 10k -L http://localhost:8080
+wrk -s get.lua -c 4 -d 20 -t 4 -R 10k -L http://localhost:8080
 
-aprof start -f 1_get_empty.svg 16932
+aprof start -f output/1_get_empty.svg "$JPID"
 wrk -s get.lua -c 4 -d $TIME -t 4 -R 10k -L http://localhost:8080 >> output/1_get_empty_wrk.txt
-aprof stop 16932
+aprof stop -f output/1_get_empty.svg "$JPID"
 
-aprof start 99 -f 2_put.svg 16932
+aprof start -f output/2_put.svg "$JPID"
 wrk -s put.lua -c 4 -d $TIME -t 4 -R 10k -L http://localhost:8080 >> output/2_put_wrk.txt
-aprof stop 16932
+aprof stop -f output/2_put.svg "$JPID"
 
-aprof start 99 -f 3_get_full.svg 16932
+aprof start -f output/3_get_full.svg "$JPID"
 wrk -s get.lua -c 4 -d $TIME -t 4 -R 10k -L http://localhost:8080 >> output/3_get_full_wrk.txt
-aprof stop 16932
+aprof stop -f output/3_get_full.svg "$JPID"
 
-aprof start 99 -f 4_delete.svg 16932
+aprof start -f output/4_delete.svg "$JPID"
 wrk -s delete.lua -c 4 -d $TIME -t 4 -R 10k -L http://localhost:8080 >> output/4_delete_wrk.txt
-aprof stop 16932
+aprof stop -f output/4_delete.svg "$JPID"
 
 #Hardcore tests
 
